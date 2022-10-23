0664a7b01421e2e000a71c6f2c027858eb50b544
rosegolden1677 <makela.adams@yahoo.com>
2022-10-22 22:36:24 -0700

    add functionality

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './List.css';
 import { AiOutlinePlus } from "react-icons/ai";
 
 const List = () => {
+
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newList = [...list,{id: new Date().getTime.toString, name: name}];
+    setList(newList)
+    setName('');
+  }
+
   return (
     <div className='add-todo'>
-      <AiOutlinePlus className='plus-icon' />
-      <input type="text" placeholder='add to-do' />
+      <form onSubmit={handleSubmit}>
+        <button type='submit'>
+          <AiOutlinePlus className='plus-icon' />
+        </button>
+        <input type="text" placeholder='add to-do' value={name} onChange={(e) =>setName(e.target.value)} />
+      </form>
+
+      {
+        list.map((item) => {
+
+          const {id, name} = item;
+
+          return (
+            <div className='list-item' key={id}>
+              <input type="checkbox" />
+              <p>{name}</p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
