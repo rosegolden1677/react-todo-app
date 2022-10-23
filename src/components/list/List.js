839fd1448d7702c8ba8944e428d7438f0354036c
rosegolden1677 <makela.adams@yahoo.com>
2022-10-22 21:52:16 -0700

    update list component

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import './List.css';
+import { AiOutlinePlus } from "react-icons/ai";
 
 const List = () => {
   return (
-    <div>List</div>
+    <div className='add-todo'>
+      <AiOutlinePlus className='plus-icon' />
+      <input type="text" placeholder='add to-do' />
+    </div>
   );
 }
 
